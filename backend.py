5f1b07403a79fd00d0903c0dfbaebe5f15cbc56d
Izzan Fakhril Islam <izzanfi@hotmail.com>
2020-06-07 12:05:40 +0700

    adding some data from backend to frontend

--- backend.py
+++ backend.py
@@ -29,20 +29,33 @@ def showIndexPage():
     )
 
 def get_datas(pika_connection, api_utils, scrapper_utils):
-    country_with_stats = []
+    overall_worldwide_stats = api_utils.get_worldwide_stats()
     country_data_list = api_utils.get_countries_list_and_codes()
     worldwide_stats = api_utils.get_worldwide_stats()
     worldwide_population_stats = scrapper_utils.get_worldwide_population_data_with_countries()
+    country_with_stats = [
+        {
+            "worldwide_population": scrapper_utils.get_total_worldwide_population(),
+            "worldwide_confirmed": overall_worldwide_stats["confirmed_case"],
+            "worldwide_recovered": overall_worldwide_stats["recovered_case"],
+            "worldwide_deaths": overall_worldwide_stats["deaths_case"]
+        }
+    ]
 
     for country_data in country_data_list:
+        country_stats = api_utils.get_stats_per_country(country_data["code"])
         country_response = {}
+        
         country_response["country_name"] = country_data["name"]
         country_response["population"] = worldwide_population_stats[country_response["country_name"]]["population"] \
             if country_response["country_name"] in list(worldwide_population_stats.keys()) else "N/A"
         country_response["area"] = worldwide_population_stats[country_response["country_name"]]["area"] \
             if country_response["country_name"] in list(worldwide_population_stats.keys()) else "N/A"
         
-        country_response.update(api_utils.get_stats_per_country(country_data["code"]))
+        country_response.update(country_stats)
+        country_response["mortality_rate"] = count_mortality_rate(
+            country_stats["deaths_case"], country_stats["confirmed_case"]
+        )
 
         country_with_stats.append(country_response)
         pika_connection.send_message(f'Retrieving Data: {int((len(country_with_stats)/len(country_data_list))*100)}%')
@@ -53,6 +66,11 @@ def get_datas(pika_connection, api_utils, scrapper_utils):
 def retrieve_data_in_background(pika_connection, api_utils, scrapper_utils):
     background_thread = threading.Thread(target=get_datas, args=(pika_connection, api_utils, scrapper_utils))
     background_thread.start()
+
+def count_mortality_rate(deaths_case, confirmed_case):
+    return f'{(int(deaths_case)/int(confirmed_case))*100}%' \
+        if (confirmed_case != 0 and confirmed_case != 'N/A') \
+             and (deaths_case != 0 and deaths_case != 'N/A') else 'N/A'
 
 
 if __name__ == '__main__':
